1f27bce72185d26453fa9a7f3dafd976a7a329bc
Sergey Pervak <pervak102kaf@mail.ru>
2025-10-30 17:13:32 +0300

    первый коммит

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Подключить БД SQLite.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Создать экземпляр SQLAlchemy и в качестве параметра
+# передать в него экземпляр приложения Flask.
+# Создать экземпляр класса SQLAlchemy и передать
+# в качестве параметра экземпляр приложения Flask.
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
+    # print(app.config)
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
